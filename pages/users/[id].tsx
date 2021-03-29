15f785d52b86e6788fb2196c6371824fd875c5fe
stevennx <steven.xu1589@gmail.com>
2021-03-28 21:55:17 -0400

    Setup NextJS + Tailwind + Typescript

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,9 +1,9 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
 
-import { User } from '../../interfaces'
-import { sampleUserData } from '../../utils/sample-data'
-import Layout from '../../components/Layout'
-import ListDetail from '../../components/ListDetail'
+import { User } from '../../src/interfaces'
+import { sampleUserData } from '../../src/utils/sample-data'
+import Layout from '../../src/components/Layout'
+import ListDetail from '../../src/components/ListDetail'
 
 type Props = {
   item?: User
