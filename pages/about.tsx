cecae82b96cdc7091521fed3ee65397162f08721
stevennx <steven.xu1589@gmail.com>
2021-03-29 16:56:54 -0400

    Fix eslint errors

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,16 @@
-import Link from 'next/link'
-import Layout from '../src/components/Layout'
+import Link from "next/link";
+import React from "react";
 
-const AboutPage = () => (
+import Layout from "../src/components/Layout";
+
+const AboutPage: React.FC = () => (
   <Layout title="About | Next.js + TypeScript Example">
     <h1>About</h1>
     <p>This is the about page</p>
     <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
+      <Link href="/">Go home</Link>
     </p>
   </Layout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link'
-import Layout from '../src/components/Layout'
+import Link from "next/link";
+import React from "react";
+
+import Layout from "../src/components/Layout";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1>Hello Next.js</h1>
     <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+      <Link href="/about">About</Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
